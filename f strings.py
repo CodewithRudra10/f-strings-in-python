fe40e41565aa96d693ff0209b58b43f229ae485a
Rudra <yadavrudra00010@gmail.com>
2025-11-14 23:18:23 +0530

    Create f strings.py

---
+++ f strings.py
@@ -0,0 +1,17 @@
+# a=(f"name: Rudra","age{15}")
+# print(a)
+
+# age=0
+# User=(input("enter the User's age"))
+# if(age<=20):
+#     print(f"In Year {2047} You will turn 100")
+# else:
+#     print(f"you will turn 100 in {2050}")
+
+
+
+10 =="rudra"
+5  =="15"
+a=(f"My Name is {10} and I am {5} years old")
+print(a)
+
